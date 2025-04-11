753c746e434143ce82d30a6228cc3faa8d1b4165
Giovani Bonadiman Goltara <giovani.goltara@gmail.com>
2025-04-11 18:59:57 +0000

    adds invoice_clean model

---
+++ models/final_table.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+SELECT
+    mc.movie_id,
+    mc.movie_title,
+    mc.genre,
+    mc.studio,
+    i.month,
+    i.location_id,
+    SUM(i.total_invoice_sum) AS rental_cost,
+    SUM(uc.total_tickets) AS tickets_sold,
+    SUM(uc.total_revenue) AS revenue
+FROM
+    {{ ref("movies_clean") }} AS mc 
+    JOIN {{ ref("invoices_clean") }} AS i  
+        ON mc.movie_id = i.movie_id
+    JOIN {{ ref("union_locations") }} AS uc 
+        ON i.location_id = uc.location_id AND i.month = uc.month
+GROUP BY
+    mc.movie_id,
+    mc.movie_title,
+    mc.genre,
+    mc.studio,
+    i.month,
+    i.location_id
---
+++ models/invoices_clean.sql
@@ -0,0 +1,11 @@
+SELECT
+    movie_id, 
+	invoice_id,
+	month,
+	LOWER(location_id),
+	studio,
+	release_date,
+	weekly_price,
+	total_invoice_sum
+FROM    
+    {{ source("silver_screen", "invoices")}}
